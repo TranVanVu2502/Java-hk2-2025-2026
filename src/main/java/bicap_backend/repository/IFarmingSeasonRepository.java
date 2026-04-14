9299cb8fe584dbf55b3e042c818aa8ecd8cbcf1f
quanminhoang <minhquan291004@gmail.com>
2026-04-14 19:02:53 +0700

    refactor: renamed from ISeasonRepository to IFarmingSeasonRepository

--- src/main/java/bicap_backend/repository/IFarmingSeasonRepository.java
+++ src/main/java/bicap_backend/repository/IFarmingSeasonRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ISeasonRepository extends JpaRepository<Season, Long> {
+public interface IFarmingSeasonRepository extends JpaRepository<Season, Long> {
 
     List<Season> findByFarmId(Long farmId);
 
