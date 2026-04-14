8d19579aec42563cbc380faa7ab6923f157c6ccc
HuySing <nguyenkimhuy030605@gmail.com>
2026-04-14 15:59:57 +0700

    Add FarmRepository - BICAP-102

--- src/main/java/bicap_backend/dto/request/FarmRequest.java
+++ src/main/java/bicap_backend/dto/request/FarmRequest.java
@@ -1,25 +1,18 @@
 package bicap_backend.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FarmRequest {
-    
-    private Long userId;
+
     private String name;
     private String address;
     private String businessLicense;
     private String ownerName;
-
-    public Long getUserId() { return userId; }
-    public void setUserId(Long userId) { this userId = userId; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this name = name; }
-
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this address = address; }
-
-    public String getBusinessLicense() { return businessLicense; }
-    public void setBusinessLicense(String businessLicense) { this businessLicense = businessLicense; }
-
-    public String getOwnerName() { return ownerName; }
-    public void setOwnerName(String ownerName) { this ownerName = ownerName; }
 }
